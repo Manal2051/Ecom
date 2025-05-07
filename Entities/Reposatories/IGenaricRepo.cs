3392cca2122d39cd398d76868851afa0781222ad
Manal <manalali2051@gmail.com>
2025-05-07 17:12:28 +0300

    Add files via upload

---
+++ Entities/Reposatories/IGenaricRepo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Reposatories
+{
+    public interface IGenaricRepo<T> where T : class
+    {
+    
+        IEnumerable<T> GetAll(Expression<Func<T,bool>>? predacate=null , string? icludeWord = null);
+
+        T GetByID(Expression<Func<T, bool>>? predacate=null, string? icludeWord = null);
+
+        void add(T item);
+        void remove(T item);
+
+        void removeRange(List<T> entities);
+       
+    }
+}
